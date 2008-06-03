fbeb3c294eda0cc081db4aba2e266b9d6ae76cd3
Srdjan Pejic <spejic@gmail.com>
2008-06-03 03:31:40 +0000

    Added a compatible version of RSpec; Created a first BDD file

--- app/spec/models/bug_spec.rb
+++ app/spec/models/bug_spec.rb
@@ -3,9 +3,23 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe Bug do
   before(:each) do
     @bug = Bug.new
+    @bug.title = @bug.description = "Testing the BDD way"
+    @bug.project_id = 1
+    @bug.severity_id = 2
   end
 
   it "should be valid" do
     @bug.should be_valid
+  end
+  
+  it "should have no errors after save" do
+    @bug.save.should be_true
+    @bug.errors.should be_empty
+  end
+  
+  it "should be logged by someone" do
+    @bug.logged_by = 1
+    @bug.save
+    @bug.logged_by.should == 1
   end
 end
