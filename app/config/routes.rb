12422dbf0dab8aa4a5d28b220b7194b73bb14d91
Srdjan Pejic <spejic@gmail.com>
2008-08-06 10:46:24 -0400

    Removing current plugin set up

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,19 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
-  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
-  map.login '/login', :controller => 'sessions', :action => 'new'
-  map.register '/register', :controller => 'users', :action => 'create'
-  map.signup '/signup', :controller => 'users', :action => 'new'
-  map.resources :users, :has_many => [ :projects, :bugs]
-
-  map.resource :session
-
-    #map.resources :users, :has_many => [ :projects, :bugs ]
  
-    map.resources :projects, :has_many => :bugs
-    
-    map.resources :bugs
-    map.resources :statuses
-    map.resources :companies
+  #map.resources :users, :has_many => [ :projects, :bugs ]
+
+  map.resources :projects, :has_many => :bugs
+  
+  map.resources :bugs
+  map.resources :statuses
+  map.resources :companies
 
   # The priority is based upon order of creation: first created -> highest priority.
 
