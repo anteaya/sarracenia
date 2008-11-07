4f4c9175dbe2c8996805f369b0a6ed7101939515
Srdjan Pejic <srdjan@loki-s2.(none)>
2008-11-07 12:05:32 +0800

    Added a new project and fixed crashy bugs along the way
    Signed-off-by: anteaya <ay.kuno@gmail.com>

--- app/app/helpers/application_helper.rb
+++ app/app/helpers/application_helper.rb
@@ -1,10 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
     def distance_of_time_in_words_from_now(to_time, include_seconds = false)
-        if Time.now < to_time
-            "in #{distance_of_time_in_words(Time.now, to_time, include_seconds)}"
-        else
-            distance_of_time_in_words_from_now(to_time) 
+        unless to_time.nil?
+            if Time.now < to_time
+                "in #{distance_of_time_in_words(Time.now, to_time, include_seconds)}"
+            else
+                distance_of_time_in_words_from_now(to_time) 
+            end
         end
     end
 end
