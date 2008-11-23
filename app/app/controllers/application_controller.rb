9a40f4a020044a6c37a453e351856d40e56dcd57
Srdjan Pejic <srdjan@loki-s2.(none)>
2008-11-23 11:29:22 +0800

    Upgraded to edge Rails; Fixed ApplicationController issue as a result;
    Signed-off-by: anteaya <ay.kuno@gmail.com>

---
+++ app/app/controllers/application_controller.rb
@@ -0,0 +1,16 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+  include AuthenticatedSystem
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => '186a71d4a5b909de989d59ef4b952d0e'
+  
+  def redirect_back_or(path)
+    redirect_to :back
+    rescue ActionController::RedirectBackError
+    redirect_to path
+  end
+end
