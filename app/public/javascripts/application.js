360dc81b032f7aaf44090d926414f1071c10ecb4
Srdjan Pejic <srdjan@loki-s2.(none)>
2008-12-08 06:50:55 +0800

    Further changes to the bug tracker part
    Signed-off-by: anteaya <ay.kuno@gmail.com>

--- app/public/javascripts/application.js
+++ app/public/javascripts/application.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $("#container").dropShadow({ color: "#aaa", opacity:0.8, left: -5, top:7, blur: 3});
+    $("#container").dropShadow({ color: "#2f2f2f", opacity:0.8, left: -5, top:7, blur: 3});
     $("#index").dropShadow({ color: "#2f2f2f", opacity:0.8, left:-5, top:7});
     $(".links").css({height: $("#bug_display").css("height")});
 });
